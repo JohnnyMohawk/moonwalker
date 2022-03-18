6c0e9115ab93616a824938a876a13b393ed620e1
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-03-18 08:56:22 -0500

    create friends data array of objects

---
+++ src/Data/friends-data.js
@@ -0,0 +1,23 @@
+export const friendsData = [
+    {
+        logo: './images/dark-matter.png',
+        link: 'https://www.darkmattercoffee.com/'
+    },
+    {
+        logo: './images/do-rite.png',
+        link: 'https://www.doritedonuts.com/'
+    },
+    {
+        logo: './images/hewn-logo.png',
+        link: 'https://www.hewnbread.com/'
+    },
+    {
+        logo: './images/CatalpaGroveFarm.jpeg',
+        link: 'http://www.catalpagrovefarm.com/'
+    },
+    {
+        logo: './images/volition.png',
+        link: 'https://www.volitiontea.com/'
+    },
+    
+]
