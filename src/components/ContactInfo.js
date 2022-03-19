8293a389a43037f6f29e1e3ea955f00a46bb2ab4
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-03-18 20:53:44 -0500

    troubleshoot deployment error

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -7,10 +7,10 @@ export default function ContactInfo(props) {
   return (
     <div className='numNaddyWrap'>
       <div className='numNaddy'>
-          <a className='heroLinks' href="https://www.google.com/maps/place/4101+W+Belmont+Ave,+Chicago,+IL+60641/@41.938798,-87.7320567,17z/data=!3m1!4b1!4m5!3m4!1s0x880fcda9b203de1f:0xe1a3936949f6334!8m2!3d41.938798!4d-87.729868" target="_blank" rel="noopennernoreferrer">
+          <a className='heroLinks' href="https://www.google.com/maps/place/4101+W+Belmont+Ave,+Chicago,+IL+60641/@41.938798,-87.7320567,17z/data=!3m1!4b1!4m5!3m4!1s0x880fcda9b203de1f:0xe1a3936949f6334!8m2!3d41.938798!4d-87.729868" target="_blank" rel="noopenner noreferrer">
               <FaMapMarkerAlt /> {props.address}
           </a>
-          <a className='heroLinks' href="tel:17736287945" target="_blank" rel="noopennernoreferrer">
+          <a className='heroLinks' href="tel:17736287945" target="_blank" rel="noopenner noreferrer">
               <FaPhoneAlt /> {props.phone}
           </a>
       </div>
