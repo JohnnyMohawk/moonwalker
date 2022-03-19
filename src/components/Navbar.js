3943a4753353ef2b54b03008640a04f00b4169c2
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-03-18 21:04:46 -0500

    troubleshoot deployment error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Lottie from 'react-lottie-player'
 import moongirl from '../../src/animations/moongirl.json'
